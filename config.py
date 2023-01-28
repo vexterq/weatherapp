124ab86b1fdf05a4869b37db3288f9b1348eed5b
vexterq <vexterq@gmail.com>
2023-01-28 16:09:42 +0100

    update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+"""This module contains the configuration for the application."""
 from dotenv import load_dotenv
 
 
 def configure():
+    """This function configures the application."""
     load_dotenv()
