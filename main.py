66994537c1930227d1581a2dabff001aca22b1d6
vexterq <vexterq@gmail.com>
2023-01-28 15:48:32 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from config import configure
+"""Main module"""
 import requests
+from config import configure
 from weather import Weather
 
-if __name__ == "__main__":
+
+def setup():
+    """Setup function"""
     configure()
-    s = requests.Session()
+    session = requests.Session()
     city_name = input("Enter a city: ")
-    weather_data = Weather.get_weather(s, city_name)
+    weather_data = Weather.get_weather(session, city_name)
     Weather.display_weather(weather_data)
+
+
+if __name__ == "__main__":
+    setup()
