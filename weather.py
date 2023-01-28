5004bb12ab3ede565c109c2b54c52820a8945a7b
vexterq <vexterq@gmail.com>
2023-01-28 16:09:26 +0100

    update weather.py

--- weather.py
+++ weather.py
@@ -1,14 +1,25 @@
+"""This module contains the Weather class."""
 import os
 
 
 class Weather:
+    """This class contains the methods to get and display weather data."""
     @staticmethod
     def get_weather(session, city_name):
+        """This method gets the weather data.
+                Args:
+                    session: The requests session.
+                    city_name: The name of the city."""
+
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={os.getenv('api_key')}"
         response = session.get(url)
         return response.json()
 
+    @staticmethod
     def display_weather(weather_data):
+        """This method displays the weather data.
+                Args:
+                    weather_data: The weather data."""
         print(
             f'Weather in {weather_data["name"]}, {weather_data["sys"]["country"]}:')
         print(
